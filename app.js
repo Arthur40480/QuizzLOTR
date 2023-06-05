9abd56fe5b4878f9a4edd5d38c2a5e6d5bf6a571
Arthur40480 <arthurgibert@free.fr>
2023-06-05 17:49:46 +0200

    Add func to compare results and add event to retrieve results

--- app.js
+++ app.js
@@ -2,19 +2,29 @@
 import question from "./question.js";
 
 // Element du DOM
-const quizzContainerElt = document.getElementsByClassName("question_container")[0];
+const quizzFormElt = document.querySelector("form");
+const quizzButtonElt = document.querySelector("button");
 
-//Création des questions/réponses du quizz de manière dynamique.
-//On utilise la méthode forEach pour parcourir notre tableau "question".
+//Tableau des résultats
+const goodResults = [];
+const playerResults = [];
+
+//On viens récupérer les bonnes réponse via "question" à l'aide de la méthode forEach
 question.forEach(function(element) {
+    goodResults.push(element.answer)
+})
+
+console.log(goodResults);
+
+//Création des questions/réponses du quizz de manière dynamique
+//On utilise la méthode forEach pour parcourir notre tableau "question" importer depuis question.js
+question.forEach(function(element, index) {
     const questionContainerElt = document.createElement("div");
     questionContainerElt.className = "question";
     const questionTitleElt = document.createElement("h2");
     questionTitleElt.innerText = element.question;
-    const questionFormElt = document.createElement("form");
-    questionFormElt.className = "form_quizz";
 
-    questionContainerElt.append(questionTitleElt, questionFormElt);
+    questionContainerElt.append(questionTitleElt);
 
     for(let i = 0; i < element.option.length; i++) {
         const divInputElt = document.createElement("div");
@@ -22,15 +32,38 @@ question.forEach(function(element) {
         const questionInputElt = document.createElement("input");
         questionInputElt.type = "radio";
         questionInputElt.id = `reponse${i + 1}`;
-        questionInputElt.value = element.option[i];
+        questionInputElt.name = `questions${index + 1}`;
+        questionInputElt.value = i + 1;
 
         const questionLabelElt = document.createElement("label");
         questionLabelElt.htmlFor = `reponse${i + 1}`;
-        questionLabelElt.innerText = element.option[i]
+        questionLabelElt.innerText = element.option[i];
 
         divInputElt.append(questionInputElt, questionLabelElt);
-        questionFormElt.append(divInputElt);
+        questionContainerElt.append(divInputElt);
+        quizzFormElt.append(questionContainerElt);
     }
-    quizzContainerElt.append(questionContainerElt);
+    
 })
 
+//Ajout d'évènement, au click on viens récupérer les données du formulaire.
+quizzButtonElt.addEventListener('click', function(event) {
+    const formData = new FormData(quizzFormElt);
+    for(let pair of formData.entries()) {
+        playerResults.push(parseInt(pair[1]));
+    }
+    console.log(playerResults);
+    compareResults(playerResults);
+});
+
+//Fonction qui compare les résultats du joueur avec notre tableau de résultat.
+function compareResults(results) {
+    for(let i = 0; i < goodResults.length; i++) {
+        if((results[i]) === goodResults[i]) {
+            console.log("Bonne réponse !")
+        }
+        else {
+            console.log("mauvaise réponse !")
+        }
+    }
+}
--- question.js
+++ question.js
@@ -2,37 +2,37 @@ const question = [
     {
         question: "Qui est l'auteur des romans sur lesquels sont basés les films ?",
         option: ["Jonah Rhys Robert Tolkien", "John Ronald Reuel Tolkien", "Jeremiah Richard Rohan Tolkien"],
-        answer: 1,
+        answer: 2,
         explanation: "L'auteur du Seigneur des Anneaux est John Ronald Reuel Tolkien. Il est principalement connu pour son travail dans le genre de la fantasy et est considéré comme l'un des auteurs les plus influents de ce genre."
     },
     {
         question: "Dans quel pays les films ont-ils été tournés ?",
         option: ["Suède", "Islande", "Nouvelle-Zélande"],
-        answer: 2,
+        answer: 3,
         explanation: "La trilogie du Seigneur des Anneaux a été principalement tournée en Nouvelle-Zélande. La beauté naturelle et diversifiée du pays en a fait un choix idéal pour recréer les paysages fantastiques de la Terre du Milieu décrits dans les romans de J.R.R. Tolkien."
     },
     {
         question: "Qui a composé la musique de la saga ?",
         option: ["Howard Shore", "Hans Zimmer", "James Horner"],
-        answer: 0,
+        answer: 1,
         explanation: "La musique du Seigneur des Anneaux a été composée par Howard Shore. Howard Shore est un compositeur canadien réputé qui a travaillé sur de nombreux films, mais il est surtout connu pour sa collaboration avec le réalisateur Peter Jackson sur la trilogie du Seigneur des Anneaux."
     },
     {
         question: "Combien d'Oscars la trilogie a-t-elle remporté au total  ?",
         option: [11, 17, 20],
-        answer: 1,
+        answer: 2,
         explanation: "La trilogie du Seigneur des Anneaux a remporté un total de 17 Oscars. Le premier film, La Communauté de l'Anneau, sorti en 2001, a remporté 4 Oscars, le deuxième film, Les Deux Tours, sorti en 2002, a remporté 2 Oscars, enfin, le troisième film, Le Retour du Roi, sorti en 2003, a été le plus grand succès de la trilogie aux Oscars, remportant 11 statuettes."
     },
     {
         question: "Quel âge fête Bilbon Sacquet, au début de La Communauté de l'Anneau ?",
         option: [71, 91, 111],
-        answer: 2,
+        answer: 3,
         explanation: "Bilbon Sacquet fête ses 111 ans, grand âge qui étonne Gandalf Le Gris, puisqu'il ne les fait pas ! 60 ans se sont passés depuis les évènements décrits dans Le Hobbit."
     },
     {
         question: "Durant quelle période se situent les aventures de Frodon Sacquet ?",
         option: ["Premier Âge", "Deuxième Âge", "Troisième Âge"],
-        answer: 2,
+        answer: 3,
         explanation: "Les événements du Seigneur des Anneaux (et aussi du Hobbit) se passe lors du Troisième Âge, culminant avec la destruction de l'Anneau unique. Le Premier âge désigne tout ce qui précède la chute de Morgoth et le Second âge tous les événements suivants jusqu'à la semi-défaite de Sauron."
     },
     {
@@ -42,19 +42,19 @@ const question = [
             "Un Anneau pour les soumettre. Un Anneau pour les gouverner. Un Anneau pour les amener tous et dans les ténèbres les lier.", 
             "Un Anneau pour les trouver. Un Anneau pour les posséder. Un Anneau pour les amener tous et dans les ténèbres les lier."
         ],
-        answer: 0,
+        answer: 1,
         explanation: "Trois Anneaux pour les rois elfes sous le ciel, Sept pour les seigneurs nains dans leurs demeures de pierre, Neuf pour les hommes mortels destinés au trépas, Un pour le Seigneur des Ténèbres sur son sombre trône, Au pays de Mordor où s'étendent les ombres Un Anneau pour les gouverner tous, Un Anneau pour les trouver, Un Anneau pour les amener tous, Et dans les ténèbres les lier Au pays de Mordor où s'étendent les ombres."
     },
     {
         question: "Sur quel fleuve le reste de la Communauté navigue-t-il avant sa dissolution ?",
         option: ["Celdin", "Anduin", "Isen"],
-        answer: 1,
+        answer: 2,
         explanation: "La Communauté de l'Anneau poursuit sa route à bord d'embarcations sur le fleuve Anduin avant de se faire traquer par les Uruk-hai envoyés par Saroumane."
     },
     {
         question: "Quel âge Aragorn a-t-il lors des événements décrits dans le Seigneur des Anneaux ? ",
         option: [39, 60, 87],
-        answer: 2,
+        answer: 3,
         explanation: "Aragorn a 87 ans lorsqu'il rencontre Frodon. En tant que descendant des Rois de Númenor, son espérance de vie est plus longue que celle d'un homme normal. (Le livre indique qu'il meurt à 210 ans.)"
     },
     {
@@ -64,7 +64,7 @@ const question = [
             "Les terres sur lesquelles s'affrontent deux armées", 
             "Le territoire Elfe sur lequel règne Elrond"
         ],
-        answer: 1,
+        answer: 2,
         explanation: "Les Champs du Pelennor désignent une plaine fertile parsemée de vergers et de fermes, autour de Minas Tirith. C'est là qu'a lieu la plus importante bataille de la Guerre de l'Anneau."
     },
 ]
