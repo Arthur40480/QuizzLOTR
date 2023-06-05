189bc772d698f2e8b247543a29620bd559d3babc
Arthur40480 <arthurgibert@free.fr>
2023-06-05 20:29:23 +0200

    Add func color and explanation questions

--- app.js
+++ app.js
@@ -4,23 +4,26 @@ import question from "./question.js";
 // Element du DOM
 const quizzFormElt = document.querySelector("form");
 const quizzButtonElt = document.querySelector("button");
+const quizzMessageElt = document.querySelector("p");
 
 //Tableau des résultats
 const goodResults = [];
 const playerResults = [];
 
+//Score
+let score = 0;
+
 //On viens récupérer les bonnes réponse via "question" à l'aide de la méthode forEach
 question.forEach(function(element) {
     goodResults.push(element.answer)
-})
-
-console.log(goodResults);
+});
 
 //Création des questions/réponses du quizz de manière dynamique
 //On utilise la méthode forEach pour parcourir notre tableau "question" importer depuis question.js
 question.forEach(function(element, index) {
     const questionContainerElt = document.createElement("div");
     questionContainerElt.className = "question";
+
     const questionTitleElt = document.createElement("h2");
     questionTitleElt.innerText = element.question;
 
@@ -31,7 +34,6 @@ question.forEach(function(element, index) {
 
         const questionInputElt = document.createElement("input");
         questionInputElt.type = "radio";
-        questionInputElt.id = `reponse${i + 1}`;
         questionInputElt.name = `questions${index + 1}`;
         questionInputElt.value = i + 1;
 
@@ -44,26 +46,63 @@ question.forEach(function(element, index) {
         quizzFormElt.append(questionContainerElt);
     }
     
-})
+    const explanationQuestionElt = document.createElement("p");
+    explanationQuestionElt.className = "explanation";
+    explanationQuestionElt.innerHTML = element.explanation;
+    explanationQuestionElt.style.display = "none";
+
+    questionContainerElt.append(explanationQuestionElt);
+});
 
 //Ajout d'évènement, au click on viens récupérer les données du formulaire.
 quizzButtonElt.addEventListener('click', function(event) {
     const formData = new FormData(quizzFormElt);
+
     for(let pair of formData.entries()) {
         playerResults.push(parseInt(pair[1]));
     }
-    console.log(playerResults);
-    compareResults(playerResults);
+
+    if(playerResults.length < 9) {
+        quizzMessageElt.innerHTML = 'Veuillez répondre à <span>TOUTES</span> les questions ! 😱';
+        return;
+    }
+    else {
+        compareResults(playerResults);
+    }
+    
 });
 
+//On récupère les éléments du DOM précèdement créer
+const allQuestionContainerElt = document.querySelectorAll(".question");
+const allInputQuizzElt = document.querySelectorAll('input[type="radio"]');
+const allExplanationQuestionElt = document.querySelectorAll(".explanation");
+
 //Fonction qui compare les résultats du joueur avec notre tableau de résultat.
+//Change la couleur en fonction du résultat.    
 function compareResults(results) {
     for(let i = 0; i < goodResults.length; i++) {
         if((results[i]) === goodResults[i]) {
-            console.log("Bonne réponse !")
+            console.log("Bonne réponse !");
+            allQuestionContainerElt[i].style.backgroundImage = "linear-gradient(to right, #a8ff78, #78ffd6)";
+            score++;
         }
         else {
-            console.log("mauvaise réponse !")
+            console.log("mauvaise réponse !");
+            allQuestionContainerElt[i].style.backgroundImage = "linear-gradient(to right, #f5567b, #fd674c)";
+            allExplanationQuestionElt[i].style.display = "block";   
         }
     }
+};
+
+//Fonction permettant de remettre à jour les couleurs.
+function resetColor(e) {
+    const index = e.target.getAttribute("name").slice(9);
+    const targetedQuestionContainerElt = allQuestionContainerElt[index - 1];
+
+    targetedQuestionContainerElt.style.backgroundColor = "#F1F1F1";
+    targetedQuestionContainerElt.style.backgroundImage = "none";
 }
+
+//On ajoute un evenement sur chacun des inputs.
+allInputQuizzElt.forEach(input => input.addEventListener("input", resetColor));
+
