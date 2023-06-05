fbd1d6c37126cc458a4af37a3df22466939e93f2
Arthur40480 <arthurgibert@free.fr>
2023-06-05 10:13:02 +0200

    Add page styling

--- app.js
+++ app.js
@@ -8,24 +8,28 @@ const quizzContainerElt = document.getElementsByClassName("question_container")[
 //On utilise la méthode forEach pour parcourir notre tableau "question".
 question.forEach(function(element) {
     const questionContainerElt = document.createElement("div");
+    questionContainerElt.className = "question";
     const questionTitleElt = document.createElement("h2");
     questionTitleElt.innerText = element.question;
     const questionFormElt = document.createElement("form");
+    questionFormElt.className = "form_quizz";
 
     questionContainerElt.append(questionTitleElt, questionFormElt);
 
     for(let i = 0; i < element.option.length; i++) {
+        const divInputElt = document.createElement("div");
+
         const questionInputElt = document.createElement("input");
         questionInputElt.type = "radio";
         questionInputElt.id = `reponse${i + 1}`;
         questionInputElt.value = element.option[i];
-        console.log()
 
         const questionLabelElt = document.createElement("label");
         questionLabelElt.htmlFor = `reponse${i + 1}`;
         questionLabelElt.innerText = element.option[i]
 
-        questionFormElt.append(questionInputElt, questionLabelElt);
+        divInputElt.append(questionInputElt, questionLabelElt);
+        questionFormElt.append(divInputElt);
     }
     quizzContainerElt.append(questionContainerElt);
 })
